6f107bf22f9a6428cfd5cdef4cdc4f586d1d224f
Agbonyin Clement <agbonyinclement@gmail.com>
2026-05-27 13:00:30 -0500

    Api Link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,9 @@ const Navbar = () => {
 
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/employee/fetch">Navbar
-
-      
-    </a>
+    <Link className="navbar-brand" to="/employee/fetch">
+  Navbar
+</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
