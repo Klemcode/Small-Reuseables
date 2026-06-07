696d18eb26d5605eb8563a56806ba0f061a8ade1
Agbonyin Clement <agbonyinclement@gmail.com>
2026-06-07 23:09:56 +0100

    Okay Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
-import appReducer from './redux/appSlice';
+import appReducer from './Redux/AppSlice';
 
 export const store = configureStore({
   reducer: {
